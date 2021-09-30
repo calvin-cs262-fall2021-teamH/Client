3b744ad04defa5bf452c4390f3fb4f5b7bf97847
paulsamdick <psd22@students.calvin.edu>
2021-09-29 22:43:52 -0400

    Updated App to include new logo.

--- App.js
+++ App.js
@@ -6,28 +6,46 @@ Brian Langejans, David Reidsma, David Heynen, Paul Dick, Kurt Wietelmann
 
 import React from 'react';
 import { Image,  StyleSheet, Text, View } from 'react-native';  
-import logo from './assets/hclogoV1.png';                       // import prototype logo
+import logo from './assets/Calvinsource.png';                       // import prototype logo
 
 // Display the prototype logo, small amount of text, and background color
 // Adapted code from: https://docs.expo.dev/tutorial/image/
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.frontScreen}>
+      <View style = {styles.logo}>
       <Image source={logo} style={{ width: 305, height: 300 }} />
-
-      <Text style={{color: '#fff', fontSize: 18}}> 
-        Created by team HUH?!
+      <Text style={styles.text}> 
+        Team Huh!?
       </Text>
+      </View>
+      
+      
+      <View style = {styles.letsGetStarted}>
+        {/*this is where the button stuff will go*/}
+
+      </View>
     </View>
   );
 }
 
 // Styles for prototype #1
 const styles = StyleSheet.create({
-  container: {
+  frontScreen: {
     flex: 1,
     backgroundColor: '#8C2032',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    paddingTop: 80,
+    paddingHorizantal:20
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold'
+    
+  },
+  letsGetStarted:{}
+
 });
---
+++ components/button.js
@@ -0,0 +1,20 @@
+import REact from 'react';
+import {View, Text, StyleSheet } from 'react-native';
+const button = () => {
+    return(
+        <View>
+            <View style = {styles.item}>
+
+            </View>
+            <Text>
+                "Hi There"
+            </Text>
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create ({
+
+});
+export default button;
