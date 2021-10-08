e47b5a6c193d7427be11cfe1dfcd05eb689080a0
Brian Langejans <bel3@students.calvin.edu>
2021-10-08 01:04:52 -0400

    Added point of interest to the map and new information screens

---
+++ screens/pointInfo.js
@@ -0,0 +1,20 @@
+/* pointInfo.js  Screen that contains information about the point selected.
+Team HUH?!
+10/7/2021
+Brian Langejans, David Reidsma, David Heynen, Paul Dick, Kurt Wietelmann
+*/
+
+import { TabRouter } from '@react-navigation/routers';
+import React, { useState } from 'react';
+import { Image, Button, View, Text, TouchableOpacity, FlatList, ImageBackground } from 'react-native';
+import { globalStyles } from '../styles/global';
+
+export default function PointInfoScreen({ route, navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'left', backgroundColor: '#8C2032' }}>
+            <Text style={{ fontSize: 30, color: "#fff", padding: 20 }}>{ route.params.name }</Text>
+            <ImageBackground source = { require('../assets/' + route.params.image) } style = { globalStyles.map }/>
+            <Text style={{ fontSize: 20, color: "#fff", padding: 20 }}>{ route.params.description }</Text>
+        </View>
+    );
+}
---
+++ styles/global.js
@@ -0,0 +1,23 @@
+/* global.js has a style sheet for various components of the Hello Campus app.
+Team HUH?!
+10/7/2021
+Brian Langejans, David Reidsma, David Heynen, Paul Dick, Kurt Wietelmann
+*/
+
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+    mapPoint: {
+        width: 50,
+        height: 50,
+        borderRadius: 100,
+        backgroundColor: 'yellow',
+        borderWidth: 3,
+        borderColor: '#ced20c',
+        opacity: 0.5,
+    },
+    map: {
+        width: 400, 
+        height : 400,
+    }
+});
