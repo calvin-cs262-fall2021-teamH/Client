71a4480225a74cd777f9e7c492a499922ee51d35
Brian Langejans <bel3@students.calvin.edu>
2021-10-24 00:20:18 -0400

    Fixed image issue in pointInfo

--- screens/map.js
+++ screens/map.js
@@ -13,16 +13,16 @@ import { globalStyles } from '../styles/global';
 export default function MapScreen({navigation}) {
   // a list of different locations on the map (only 2 for this prototype)
   const locations = [
-    { name: "Whiskey Pond", image: 'WhiskeyPond.png',
-        description: "This secluded pond is fed by a seep on the eastern edge. It is home to ducks," + 
-                      "frogs, and plants like Buttonbush, Duckweed, and the tiniest vascular" +
-                      "plant in Michigan, water meal. Watch for the Great Blue Heron that often feeds here"},
-    { name: "Crown Gap", image: 'CrownGap.png',
-        description: "In 1995, this large maple tree fell, removing branches from several neighboring trees. " +
-                     "The result was a large hole in the canopy, or a crown gap. The gap allows more sunlight to " +
-                     "reach the forest floor, encouraging growth of seedlings. Eventually one or two of the seedlings " +
-                     "you see now will out-compete the others and will fill the canopy gap"},
-  ];  
+    { name: 'Whiskey Pond', image: require('../assets/WhiskeyPond.png'),
+        description: 'This secluded pond is fed by a seep on the eastern edge. It is home to ducks,' + 
+                      'frogs, and plants like Buttonbush, Duckweed, and the tiniest vascular' +
+                      'plant in Michigan, water meal. Watch for the Great Blue Heron that often feeds here'},
+    { name: 'Crown Gap', image: require('../assets/CrownGap.png'),
+        description: 'In 1995, this large maple tree fell, removing branches from several neighboring trees. ' +
+                     'The result was a large hole in the canopy, or a crown gap. The gap allows more sunlight to ' +
+                     'reach the forest floor, encouraging growth of seedlings. Eventually one or two of the seedlings ' +
+                     'you see now will out-compete the others and will fill the canopy gap'},
+  ];
 
   return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#8C2032' }}>
