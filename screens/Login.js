3511116ee8fdff267ea64944da8a0709bada8856
paulsamdick <90645514+paulsamdick@users.noreply.github.com>
2021-11-02 21:07:46 -0400

    Committing the fixed login screen

--- screens/Login.js
+++ screens/Login.js
@@ -8,14 +8,14 @@ const LoginScreen = ({ navigation }) => {
     console.log("LoginScreen.js 6 | loggin in");
     try {
       const { type, user } = await Google.logInAsync({
-        iosClientId: 2260489795-nvs04mkpqbhrjbd7ne2jb560e2a3dhdm.apps.googleusercontent.com,
-        androidClientId: 2260489795-b82e25fatl0ih72e43ii5q6q858fb6ql.apps.googleusercontent.com,
+        iosClientId: "2260489795-nvs04mkpqbhrjbd7ne2jb560e2a3dhdm.apps.googleusercontent.com",
+        androidClientId: "2260489795-b82e25fatl0ih72e43ii5q6q858fb6ql.apps.googleusercontent.com",
       });
 
       if (type === "success") {
         // Then you can use the Google REST API
         console.log("LoginScreen.js 17 | success, navigating to profile");
-        navigation.navigate("Profile", { user });
+        navigation.navigate("ProfileScreen", { user });
       }
     } catch (error) {
       console.log("LoginScreen.js 19 | error with login", error);
