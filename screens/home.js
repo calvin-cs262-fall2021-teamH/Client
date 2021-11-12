002131d1ad05ad8d309e6e6a70875c85019936b3
kwietelmann <kaw57@students.calvin.edu>
2021-11-12 15:38:25 -0500

    formatting home.js with 4-space tabs for lab 11

--- screens/home.js
+++ screens/home.js
@@ -9,53 +9,53 @@ import React, { useState } from 'react';
 import { Image, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function HomeScreen({navigation}) {
-    return (
-        <View style={styles.container}>
-          
-          <Image source={ require('../assets/truelogow_o_background.png')} style={{ width: 300, height: 300 }} />
-          
-          <TouchableOpacity style={globalStyles.genericButton} onPress={() => navigation.navigate('Map')}>
-            <Text style={styles.loginText}>Take a Tour!</Text>
-          </TouchableOpacity>
+export default function HomeScreen({ navigation }) {
+	return (
+		<View style={styles.container}>
 
-          <TouchableOpacity style={globalStyles.genericButton} onPress={() => navigation.navigate('Login')}>
-            <Text style={styles.loginText}>Sign In</Text>
-          </TouchableOpacity>
+			<Image source={require('../assets/truelogow_o_background.png')} style={{ width: 300, height: 300 }} />
 
-          <TouchableOpacity style={styles.aboutButton} onPress={() => navigation.navigate('About')}>
-            <Text style={styles.aboutText}>About</Text>
-          </TouchableOpacity>
-        </View>
-    );
+			<TouchableOpacity style={globalStyles.genericButton} onPress={() => navigation.navigate('Map')}>
+				<Text style={styles.loginText}>Take a Tour!</Text>
+			</TouchableOpacity>
+
+			<TouchableOpacity style={globalStyles.genericButton} onPress={() => navigation.navigate('Login')}>
+				<Text style={styles.loginText}>Sign In</Text>
+			</TouchableOpacity>
+
+			<TouchableOpacity style={styles.aboutButton} onPress={() => navigation.navigate('About')}>
+				<Text style={styles.aboutText}>About</Text>
+			</TouchableOpacity>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#8C2131",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#8C2131",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 
-  loginText: {
-    fontWeight: 'bold',
-    color: "#000000"
-  },
+	loginText: {
+		fontWeight: 'bold',
+		color: "#000000"
+	},
 
-aboutButton: {
-  width : "80%",
-  borderRadius: 75,
-  height: 75,
-  alignItems: "center",
-  justifyContent: "center",
-  marginTop: 40,
-  backgroundColor: "#8C2131",
+	aboutButton: {
+		width: "80%",
+		borderRadius: 75,
+		height: 75,
+		alignItems: "center",
+		justifyContent: "center",
+		marginTop: 40,
+		backgroundColor: "#8C2131",
 
-},
+	},
 
-aboutText: {
-  fontWeight: 'bold',
-  color: "#d9aa00"
-},
+	aboutText: {
+		fontWeight: 'bold',
+		color: "#d9aa00"
+	},
 });
