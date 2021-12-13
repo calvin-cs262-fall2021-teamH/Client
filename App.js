336229f3234d1c58a29a6ffcf122e5330ffd77d4
David Reidsma <david.reidsma@gmail.com>
2021-12-13 02:29:48 -0500

    reverted unneeded changes

--- App.js
+++ App.js
@@ -8,9 +8,14 @@ adapted from the navigation tutorial found at: https://reactnavigation.org/docs/
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from "./screens/home";
 import MapScreen from "./screens/map";
+import GeoPrototype from './screens/GeolocationPrototype';
 import PointInfoScreen from './screens/pointInfo';
+
+//import Header from './shared/header';
+import Login from './screens/Login';
 import About from './screens/about';
 import DrawerScreen from './screens/Navigator2'
 import myStudents from './screens/myStudents'
@@ -21,118 +26,112 @@ import SignedOutLocationList from './screens/signedOutLocations';
 import addStudentsToCourse from './screens/addStudent';
 import AddQuestionsScreen from './screens/addQuestions';
 import AddLocationScreen from './screens/addLocation';
-import { LogBox } from 'react-native';
 //import Icon from 'react-native-ionicons';
 
+
 import ListScreen from './screens/list';
+
 import Prompt from './screens/prompt';
-import { TouchableOpacity, Text, Header } from 'react-native';
+import { TouchableOpacity , Text, Header} from 'react-native';
 
-
-LogBox.ignoreAllLogs(); //Disables warnings
 
 const Stack = createNativeStackNavigator();
+const Stack1= createNativeStackNavigator();
+
+
+
+console.disableYellowBox = true;
+
 function App() {
 
-	Stack.navigationOptions = (
-		{
-			headerLeft: () => (
-				<TouchableOpacity
-					onPress={() => alert('Left Menu Clicked')}
-					style={{ marginLeft: 10 }}>
-					<Text style={{ color: 'white' }}>Left Menu</Text>
-				</TouchableOpacity>
-			)
-		}
-	)
-	return (
-		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Home">
-				<Stack.Screen
-					name="Home"
-					component={HomeScreen}
-					options={{
-						headerShown: true,
-						headerStyle: {
-							backgroundColor: '#fff',
-						},
-						headerTintColor: '#8C2131',
-						headerTitleStyle: {
-							fontWeight: 'bold',
-						},
-					}}
-				/>
-				<Stack.Screen
-					name="Map"
-					component={MapScreen}
-					options={{
-						headerShown: true,
-						headerTitleAlign: "center",
-						headerTintColor: "maroon",
-					}
-					}
-
-				/>
-				<Stack.Screen
-					name="PointInfo"
-					component={PointInfoScreen}
-					options={{
-						headerShown: true,
-						headerTitleAlign: "center",
-						headerTintColor: "maroon",
-					}}
-				/>
-				<Stack.Screen name="About" component={About} options={{
-					headerShown: true,
-					headerTitleAlign: "center",
-					headerTintColor: "maroon",
-				}} />
-				<Stack.Screen name="Questions" component={QuestionScreen} options={{
-					headerShown: true,
-					headerTitleAlign: "center",
-					headerTintColor: "maroon",
-				}} />
-				<Stack.Screen name="Location" component={ListScreen} options={{
-					headerShown: true,
-					headerTitleAlign: "center",
-					headerTintColor: "maroon",
-				}} />
-				<Stack.Screen name="Prompt" component={Prompt} options={{
-					headerShown: true,
-					headerTitleAlign: "center",
-					headerTintColor: "maroon",
-				}} />
-				<Stack.Screen name="Navigator2" component={DrawerScreen} options={{
-					headerShown: true,
-					headerTitleAlign: "center",
-					headerTintColor: "maroon",
-				}} />
-				<Stack.Screen name="Your Students" component={myStudents} options={{
-					headerShown: true,
-					headerTitleAlign: "center",
-					headerTintColor: "maroon",
-				}} />
-				<Stack.Screen name="Dashboard" component={myDashBoard} options={{
-					headerShown: true,
-					headerTitleAlign: "center",
-					headerTintColor: "maroon",
-				}} />
-				<Stack.Screen name="Location List" component={locationQuestion} options={{
-					headerShown: true,
-					headerTitleAlign: "center",
-					headerTintColor: "maroon",
-				}} />
-				<Stack.Screen name="Points of Interest" component={SignedOutLocationList} options={{
-					headerShown: true,
-					headerTitleAlign: "center",
-					headerTintColor: "maroon",
-				}} />
-				<Stack.Screen name="Add Students" component={addStudentsToCourse} options={{ headerShown: false }} />
-				<Stack.Screen name="Add Question" component={AddQuestionsScreen} options={{ headerShown: true }} />
-
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  Stack.navigationOptions =(
+  {
+      headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => alert('Left Menu Clicked')}
+              style={{marginLeft: 10}}>
+              <Text style={{color: 'white'}}>Left Menu</Text>
+            </TouchableOpacity>
+          )
+      }
+  )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={{ headerShown: true,
+            headerStyle: {
+              backgroundColor: '#fff',
+            },
+            headerTintColor: '#8C2131',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+          />
+        <Stack.Screen 
+          name="Map" 
+          component={MapScreen}
+          options={{ headerShown: true,
+          headerTitleAlign: "center",
+          headerTintColor: "maroon",
+        }
+          }
+          
+          />
+        <Stack.Screen 
+          name="PointInfo" 
+          component={PointInfoScreen}
+          options={{ headerShown: true,
+            headerTitleAlign: "center",
+            headerTintColor: "maroon",
+          }}
+          />
+        <Stack.Screen name = "About" component = {About} options={{ headerShown: true,
+          headerTitleAlign: "center",
+          headerTintColor: "maroon",
+        }}/>
+        <Stack.Screen name = "Questions" component = {QuestionScreen} options={{ headerShown: true,
+          headerTitleAlign: "center",
+          headerTintColor: "maroon",
+        }}/>
+        <Stack.Screen name = "Location" component = {ListScreen} options={{ headerShown: true,
+          headerTitleAlign: "center",
+          headerTintColor: "maroon",
+        }}/>
+        <Stack.Screen name = "Prompt" component = {Prompt} options={{ headerShown: true,
+          headerTitleAlign: "center",
+          headerTintColor: "maroon",
+        }}/>
+        <Stack.Screen name = "Navigator2" component = {DrawerScreen} options={{ headerShown: true,
+          headerTitleAlign: "center",
+          headerTintColor: "maroon",
+        }}/>
+        <Stack.Screen name = "My Students" component = {myStudents} options={{ headerShown: true,
+          headerTitleAlign: "center",
+          headerTintColor: "maroon",
+        }} />
+        <Stack.Screen name = "Dashboard" component = {myDashBoard} options={{ headerShown: true,
+          headerTitleAlign: "center",
+          headerTintColor: "maroon",
+        }}/>
+        <Stack.Screen name = "Location List" component = {locationQuestion} options={{ headerShown: true,
+          headerTitleAlign: "center",
+          headerTintColor: "maroon",
+        }}/>
+        <Stack.Screen name = "Points of Interest" component = {SignedOutLocationList} options={{ headerShown: true,
+          headerTitleAlign: "center",
+          headerTintColor: "maroon",
+        }}/>
+        <Stack.Screen name = "Add Students" component = {addStudentsToCourse} options= {{headerShown: false}}/>
+        <Stack.Screen name = "Add Question" component = {AddQuestionsScreen}/>
+        <Stack.Screen name = "Add Location" component = {AddLocationScreen}/>
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
