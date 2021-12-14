cb691fa91270382dbe0dd579552219c217b4303d
Brian Langejans <bel3@students.calvin.edu>
2021-12-13 20:05:20 -0500

    Update My students screen
    Now does not cause so many warnings!

--- screens/myStudents.js
+++ screens/myStudents.js
@@ -23,29 +23,8 @@ import {
   TextInput,
   RefreshControl,
 } from "react-native";
-
-import {
-  FontAwesome,
-  SolidIcons,
-  RegularIcons,
-  BrandIcons,
-  parseIconFromClassName,
-}
-  from 'react-native-fontawesome';
 import filter from 'lodash.filter'
 import { globalStyles } from "../styles/global";
-import * as Google from "expo-google-app-auth";
-import { useRoute } from '@react-navigation/native';
-import { AsyncStorage } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import {
-	HeaderButtons,
-	HeaderButton,
-	Item
-} from 'react-navigation-header-buttons';
-import { checkIfTokenExpired, refreshAuthAsync, getCachedAuthAsync, authState } from './home';
-//import @react-native-async-storage/async-storage;
-
 
 export default function myStudents({ route, navigation }) {
   const [isLoading, setIsLoading] = useState(true);
@@ -53,23 +32,7 @@ export default function myStudents({ route, navigation }) {
   const [fullData, setFullData] = useState([]);
   const [thoseWhoAreNotStudents, setThoseWhoAreNotStudents] = useState([]);
   const [refreshPage, setRefreshPage] = useState([]);
-
-  //const [error, setError] = useState(null);
   const [error, setError] = useState([]);
-  //const [isLoading, setIsLoading] = useState(false);
-  /*useEffect(() => {
-      fetch('https://hello-campus.herokuapp.com/users/')
-          .then((response) => response.json())
-          .then((json) => {setData(json)
-              setFullData(response.results);
-
-               setIsLoading(false)})
-          .catch((error) => console.error(error))
-          .finally(() => setLoading(false));
-    }, []);*/
-
-
-
 
   useEffect(() => {
     setIsLoading(true);
@@ -79,7 +42,6 @@ export default function myStudents({ route, navigation }) {
       .then((json) => {
         setData(json);
 
-        // ADD THIS
         setFullData(json);
 
         setIsLoading(false);
@@ -149,9 +111,6 @@ export default function myStudents({ route, navigation }) {
   };
 
   function removeStudent(email) {
-    console.log(email);
-    console.log(setRefreshPage);
-
     fetch(`https://hello-campus.herokuapp.com/updateStudentStatus/`,
       {
         method: 'PUT',
@@ -171,8 +130,6 @@ export default function myStudents({ route, navigation }) {
     setData(filteredData);
   }
 
-
-
   return (
     <ImageBackground source={require('../assets/good.jpg')}
       style={{
@@ -184,7 +141,7 @@ export default function myStudents({ route, navigation }) {
       <Text style={{ fontSize: 18, fontWeight: 'bold', color: "#8C2131", marginTop: 30 }} >
         Select a student to view their answers.
       </Text>
-      <ScrollView>
+      <View>
         <FlatList
           data={data}
           keyExtractor={({ id }, index) => id.toString()}
@@ -200,18 +157,17 @@ export default function myStudents({ route, navigation }) {
                 <TouchableOpacity onPress={() => { removeStudent(item.email), deleteItemByEmail(item.email) }}>
                   <Text style={{ color: "#fff", fontSize: 18, borderColor: "#fff", borderWidth: 1, backgroundColor: "maroon", margin: 25, borderRadius: 20 }}> X </Text>
                 </TouchableOpacity>
-
               </View>
             </View>
           )}
 
         />
-      </ScrollView>
+      </View>
       <View style={{alignItems:"center", marginBottom:10}}>
         <TouchableOpacity
           style={styles.AddButtonStyle}
           onPress={() =>
-            navigation.navigate("Add Students", { name: route.params.name })}//{() => setModalVisible(true)}//////
+            navigation.navigate("Add Students", { name: route.params.name })}
         >
           <Text style={styles.textStyle}> Add Student </Text>
         </TouchableOpacity>
